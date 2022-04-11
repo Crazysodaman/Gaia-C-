6dc616139d77d5f1e0e2237c93bc70cf97712379
Crazysodaman <77500555+Crazysodaman@users.noreply.github.com>
2022-04-11 15:15:35 -0500

    added servo variables

--- Movement.cpp
+++ Movement.cpp
@@ -1,1 +1,12 @@
-#include "Movement.h"
+class Movement
+{
+  int FRR = 8, FLR = 24, FRT = 7, FLT= 23, FRFT = 6, FLFT = 22, CRR = 5, CLR = 21, CRT = 4, CLT = 20, CRFT = 3, CLFT = 19, BRR = 2, BLR = 18, BRT = 1, BLT = 17, BRFT = 0, BLFT = 16;
+  int aspr = [0, 1, 2, 3, 4, 5, 6, 7, 8];  \\all servo positions right
+  int aspl = [16, 17, 18, 19, 20, 21, 22, 23, 24];  \\ all servo positions left
+  int gar = [2, 21, 8];
+  int gat = [1, 20, 7];
+  int gaf = [0, 19, 6];
+  int gbr = [24, 5, 18];
+  int gbt = [23, 4, 17];
+  int gbf = [22, 3, 16];
+};
